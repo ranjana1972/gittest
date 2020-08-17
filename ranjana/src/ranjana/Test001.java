d316a2ed1e1870872d1ecb039864506319af338a
15712 <15712@laptop1>
2020-08-16 20:02:45 -0400

    this is my test 2

---
+++ ranjana/src/ranjana/Test001.java
@@ -0,0 +1,19 @@
+package ranjana;
+
+public class Test001 {
+
+	public static void main(String[] args) {
+		//user is greater than 21
+		int a = 10;
+		char cs= 'u';
+		if ( cs=='u')
+				if(a<=21){
+			System.out.println("user is elegible to vote");
+		}
+				else System .out .println("user is not elegible to vote because they are not cs");	
+	
+				
+
+	}
+
+}
---
+++ ranjana/src/ranjana/test002.java
@@ -0,0 +1,16 @@
+package ranjana;
+
+public class test002 {
+
+	public static void main(String[] args) {
+		int a = 100;
+		int b = 7;
+		int c= 6;
+		while (a>=b)
+		System.out.println("current value of a");
+		
+		  
+
+	}
+
+}
